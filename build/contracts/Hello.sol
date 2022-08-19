4f5e359e2f4289b3a05a671fa6a67454f5aaac95
johnny evans <evanseburu32@gmail.com>
2022-08-19 16:47:37 +0100

    init

--- build/contracts/Hello.sol
+++ build/contracts/Hello.sol
@@ -1,3 +1,12 @@
-contract {
-    string heloo = 'hello'
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Hello {
+    uint256 public counter;
+
+    function add() public returns  (uint256){
+        counter += 1;
+        return counter;
+    }
 }
